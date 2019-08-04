f9398eca4abccf3b7eb6c9089677b80e20488699
Peregurin <chinmayvirmani@gmail.com>
2019-08-04 20:54:08 +0530

    SDL init succeeded, external dependencies added

--- Particle-Fire-Explosion/Particle-Fire-Explosion/Source.cpp
+++ Particle-Fire-Explosion/Particle-Fire-Explosion/Source.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
+#include<SDL.h>
 using namespace std;
 
-int main() {
+int wmain() {
 
-	cout << "Hello World" << endl;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		cout << "SDL init failed" << endl;
+		return 1;
+	}
+	cout << "SDL init succeeded" << endl;
+	SDL_Quit();
 
 	cin.get();
 

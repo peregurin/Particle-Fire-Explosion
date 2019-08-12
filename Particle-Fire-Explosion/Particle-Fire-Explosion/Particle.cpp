6ee216cd2cdffdf1c71d2b8adc6b31b834693d42
Peregurin <chinmayvirmani@gmail.com>
2019-08-12 18:02:54 +0530

    final

--- Particle-Fire-Explosion/Particle-Fire-Explosion/Particle.cpp
+++ Particle-Fire-Explosion/Particle-Fire-Explosion/Particle.cpp
@@ -5,10 +5,13 @@
 
 Particle::Particle(): m_x(0), m_y(0){
 	m_direction = (2.0 * M_PI * rand()) / RAND_MAX;
-	m_speed = (0.0005 * rand()) / RAND_MAX;
+	m_speed = (0.04 * rand()) / RAND_MAX;
+	m_speed *= m_speed;
 }
 
 void Particle::update(int interval) {
+	m_direction += interval * 0.0004;
+
 	double xspeed = m_speed * cos(m_direction);
 	double yspeed = m_speed * sin(m_direction);
 
